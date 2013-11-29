f02597bb0907dd1e5878551ac9b8713ebb5dd3a9
lordd466b <lordd466b@gmail.com>
2013-11-29 13:57:51 +0800

    made findX method a static method

--- src/SecantComputation.java
+++ src/SecantComputation.java
@@ -3,19 +3,19 @@ import static java.lang.Math.pow;
 
 public class SecantComputation {
 
-	public ArrayList<Pair> secant(ArrayList<Polynomial> Poly, double x0 , double x1, int iteration) {
-		ArrayList<Pair> Ans = new ArrayList<Pair>();
+	public ArrayList<Point> secant(ArrayList<Term> Poly, double x0 , double x1, int iteration) {
+		ArrayList<Point> Ans = new ArrayList<Point>();
 		double newX = 0;
 		int current = 0;
-		Pair pair;
+		Point pair;
 		//X0, X1
-		Ans.add(new Pair(x0, findY(Poly, x0)));
-		Ans.add(new Pair(x1, findY(Poly, x1)));
+		Ans.add(new Point(x0, findY(Poly, x0)));
+		Ans.add(new Point(x1, findY(Poly, x1)));
 		current++;
 		
 		for (int i = 2; i <= iteration; i++){
 			newX = Ans.get(current).getX() - ((Ans.get(current).getY() * (Ans.get(current - 1).getX() - Ans.get(current).getX())) / (Ans.get(current - 1).getY() - Ans.get(current).getY()));
-			Ans.add( new Pair( newX, findY(Poly, newX)));
+			Ans.add( new Point( newX, findY(Poly, newX)));
 			current++;
 		}
 		
@@ -24,10 +24,10 @@ public class SecantComputation {
 	}
 	
 	
-	public double findY( ArrayList<Polynomial> Poly, double x){
+	public static double findY( ArrayList<Term> Poly, double x){
 		double y = 0;
 		
-		for (Polynomial poly : Poly) {
+		for (Term poly : Poly) {
 			y += poly.getNumber() * pow(x, poly.getPower() );
 		}
 			
