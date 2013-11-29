cd3191c03b17d099509023fa34fd0a7e9fbed3f8
lordd466b <lordd466b@gmail.com>
2013-11-29 18:10:56 +0800

    Bisection Code and .gitignore

---
+++ src/BisectionIteration.java
@@ -0,0 +1,24 @@
+
+
+public class BisectionIteration{
+	Point a;
+	Point b;
+	Point mid;
+	
+	public BisectionIteration(Point a, Point b, Point mid){
+		this.a = a;
+		this.b = b;
+		this.mid = mid;
+	}
+	
+	public Point getA(){
+		return a;
+	}
+	public Point getB(){
+		return b;
+	}
+	public Point getMid(){
+		return mid;
+	}
+		
+}
