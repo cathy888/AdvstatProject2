0687dda90243a4c704252164db6c152cfb4f9e1f
TsugumiWinters <TsugumiWinters@github.com>
2013-12-03 05:56:31 +0800

    added table and space for graphs

--- src/views/MainView.java
+++ src/views/MainView.java
@@ -9,17 +9,20 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JScrollPane;
 import javax.swing.border.TitledBorder;
 import javax.swing.JSplitPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class MainView extends JFrame {
 	
 	private JPanel contentPane;
 	private JPanel panPolynomials;
+	private JTable table;
 	
 	public MainView() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		setMinimumSize(new Dimension(200, 100));
-		setSize(450, 300);
+		setSize(600, 400);
 		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -52,6 +55,21 @@ public class MainView extends JFrame {
 		
 		JPanel panRight = new JPanel();
 		splitPane.setRightComponent(panRight);
+		panRight.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panGraph = new JPanel();
+		panRight.add(panGraph, BorderLayout.SOUTH);
+		
+		JScrollPane scpTable = new JScrollPane();
+		panRight.add(scpTable, BorderLayout.CENTER);
+		
+		DefaultTableModel tableModel = new DefaultTableModel(new Object[][] {},
+				new String[] { "Lower Root", "Upper Root", "Middle Root",
+						"f( Lower Root )", "f( Upper Root )", "f( Middle Root )",
+						"Relative Error" });
+		
+		table = new JTable(tableModel);
+		scpTable.setViewportView(table);
 	}
 	
 }
