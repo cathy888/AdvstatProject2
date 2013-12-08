c74ce985761f23d748ccb0ce7918628ab113fcb1
TsugumiWinters <TsugumiWinters@github.com>
2013-12-08 13:01:39 +0800

    made Validation model

---
+++ src/models/InputValidation.java
@@ -0,0 +1,25 @@
+package models;
+
+import models.objects.ProjectInput;
+
+public class InputValidation {
+	
+	public InputValidation() {}
+	
+	public static boolean validateInput(ProjectInput input) {
+		if (input.getPolynomial().size() <= 0) {
+			return false;
+		}
+		
+		if (!input.getIteration().isEnabled() && !input.getThreshold().isEnabled()) {
+			return false;
+		}
+		
+		if (input.getX0() >= input.getX1()) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+}
